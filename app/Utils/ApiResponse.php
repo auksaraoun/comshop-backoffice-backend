8487302d9f352dc5c1348b04d7c9682333cec482
auksaraoun <auksaraoun@hotmail.com>
2026-03-17 19:06:53 +0700

    fix api response

--- app/Utils/ApiResponse.php
+++ app/Utils/ApiResponse.php
@@ -4,45 +4,46 @@ namespace App\Utils;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ApiResponse
 {
-    public function success(mixed $data = null, string $message = "", int $status = 200, array $extra = []): JsonResponse{
+    public function success(mixed $data = null, string $message = '', int $status = 200, array $extra = []): JsonResponse
+    {
 
         $response = [
-            "success"=> true,
-            "message"=> $message,
+            'success' => true,
+            'message' => $message,
         ];
 
-        if($data instanceof LengthAwarePaginator || $data instanceof AnonymousResourceCollection){
-            $response['data'] =  $data->items();
+        if ($data->resource instanceof LengthAwarePaginator) {
+            $response['data'] = $data->items();
             $response['meta'] = [
                 'current_page' => $data->currentPage(),
-                'per_page'     => $data->perPage(),
-                'total'        => $data->total(),
-                'last_page'    => $data->lastPage(),
+                'per_page' => $data->perPage(),
+                'total' => $data->total(),
+                'last_page' => $data->lastPage(),
             ];
-        }else{
-            $response['data'] =  $data;
+        } else {
+            $response['data'] = $data;
         }
 
         $response = array_merge($response, $extra);
 
-        return response()->json($response,$status);
+        return response()->json($response, $status);
     }
 
-    public function fail(string $message = "", int $status = 200, array $extra = []): JsonResponse {
+    public function fail(string $message = '', int $status = 200, array $extra = []): JsonResponse
+    {
 
         $response = [
-            "success"=> false,
-            "data" => null,
-            "message"=> $message,
-            "errors" => null
+            'success' => false,
+            'data' => null,
+            'message' => $message,
+            'errors' => null,
         ];
 
         $response = array_merge($response, $extra);
 
-        return response()->json($response,$status); 
+        return response()->json($response, $status);
     }
 }
